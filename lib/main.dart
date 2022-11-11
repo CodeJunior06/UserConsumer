1ce52db223e8a35bda03a24850d342bc8023ca58
CodeJunior06 <san1428.srp@gmail.com>
2022-11-11 15:40:09 -0500

    Refactor - Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.yellow,
+        
       ),
       initialRoute: Routes.HOME,
     );
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:user_consumer/services/model/user_consumer.dart';
@@ -12,6 +14,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ViewModelHome viewModelHome = ViewModelHome();
+
   late Future<List<User>?> _listUser;
   @override
   void initState() {
@@ -21,29 +24,48 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder(
-        future: _listUser,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return GridView.builder(
-                itemCount: snapshot.data?.length,
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                  return Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Card(
-                        child: Center(child: Text(snapshot.data![index].name)),
-                      ));
-                });
-          }
-          return const Center(
-            child: CircularProgressIndicator()
-          );
-        },
-      ),
-    );
+    return SafeArea(
+        child: Scaffold(
+            body: Column(
+      children: [
+        Container(
+          child: Text("USERS CONSUMER",
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w600),
+              textAlign: TextAlign.center),
+          color: Colors.lime,
+          width: double.infinity,
+        ),
+        Expanded(
+          child: FutureBuilder(
+            future: _listUser,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return GridView.builder(
+                    itemCount: snapshot.data?.length,
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            mainAxisExtent: 150, crossAxisCount: 2),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: Card(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.0))),
+                            elevation: 5.0,
+                            child: Text(snapshot.data![index].name)),
+                      );
+                    });
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        )
+      ],
+    )));
   }
 }
